4fa3fddb76a8e7cb27205791bbaa505e89ad318b
Saswata Das <65081791+SDas@users.noreply.github.com>
2024-08-03 04:47:44 +0530

    barcode scan feature added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { BillTemplateComponent } from './components/bill-template/bill-template.component';
 import { LoginComponent } from './components/welcome/login/login.component';
 import { SignupComponent } from './components/welcome/signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -17,7 +16,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    BillTemplateComponent,
     LoginComponent,
     SignupComponent,
     WelcomeComponent,
